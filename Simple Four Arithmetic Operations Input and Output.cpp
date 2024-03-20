14ad27370f3a3cb8c646b29b7e642b4bcda98d4e
file00 <51076009+file00@users.noreply.github.com>
2024-03-20 09:04:41 +0900

    Add files via upload

---
+++ Simple Four Arithmetic Operations Input and Output.cpp
@@ -0,0 +1,18 @@
+/* Simple Four Arithmetic Operations Input and Output */
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    
+    cin >> A >> B;
+    
+    cout << A+B << endl;
+    cout << A-B << endl;
+    cout << A*B << endl;
+    cout << A/B << endl;
+    cout << A%B << endl;
+
+    return 0;
+}
