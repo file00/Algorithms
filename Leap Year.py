78cc229bee7745f54bc2d98fe2af38e8f2bff97b
file00 <51076009+file00@users.noreply.github.com>
2024-03-22 12:57:48 +0900

    Add files via upload

---
+++ Leap Year.py
@@ -0,0 +1,10 @@
+# leap Year
+
+i = int(input())
+
+if i % 4 == 0 and (i % 100 != 0 or i % 400 == 0):
+    print("This year is Leap Year.")
+else:
+    print("It's not a Leap Year this year.")
+    
+# Leap Year: It means the year in which February is 29 days, not 28 days.
