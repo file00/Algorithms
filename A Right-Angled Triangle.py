ec096afcccaadb9cd7281639cedffd56fe4493d5
file00 <51076009+file00@users.noreply.github.com>
2024-04-03 09:05:57 +0900

    Update A Right-Angled Triangle.py

--- A Right-Angled Triangle.py
+++ A Right-Angled Triangle.py
@@ -2,5 +2,15 @@
 
 A = int(input())
 
-for i in range(1, (A+1)):
-    print("$"*i)
+for i in range(1,(A+1)):
+    print(" "*(A-1) + "*"*i)
+
+# Same Answer
+# for i in range(1, (A+1)):
+#    print(""*(A-1)+"*"*i)
+
+# A version that only has a different direction of output
+A = int(input())
+
+for i in range(1,(A+1)):
+    print(" "*(A-i) + "*"*i)
