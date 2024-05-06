703d911db6ca6d23b56760a3b4409764076b9576
file00 <51076009+file00@users.noreply.github.com>
2024-05-07 08:44:49 +0900

    Add files via upload

---
+++ C++ STL Stack.cpp
@@ -0,0 +1,19 @@
+/* C++ STL Stack */
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    // create a stack of strings
+    stack<string> languages;
+    
+    // add element to the Stack
+    languages.push("C++");
+    languages.push("Java");
+    languages.push("Python");
+    
+    // print top element
+    cout << languages.top();
+
+    return 0;
+}
