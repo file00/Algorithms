917f0f356a962f56d809711e14c60a4bb3a7bc00
file00 <51076009+file00@users.noreply.github.com>
2025-10-10 00:31:48 +0900

    Update Stack implementation in python.py

--- Data Structures/Stack/Stack implementation in python.py
+++ Data Structures/Stack/Stack implementation in python.py
@@ -5,17 +5,14 @@ def create_stack():
     stack = []
     return stack
 
-
 # Creating an empty stack
 def check_empty(stack):
     return len(stack) == 0
-
 
 # Adding items into the stack
 def push(stack, item):
     stack.append(item)
     print("pushed item: " + item)
-
 
 # Removing an element from the stack
 def pop(stack):
@@ -24,11 +21,11 @@ def pop(stack):
 
     return stack.pop()
 
-
 stack = create_stack()
 push(stack, str(1))
 push(stack, str(2))
 push(stack, str(3))
 push(stack, str(4))
 print("popped item: " + pop(stack))
+
 print("stack after popping an element: " + str(stack))
